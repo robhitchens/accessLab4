695524e6de08cec1863d580bb462b01433c6c0b1
roberthitchens3 <rjhitchens3@gmail.com>
2016-01-26 16:05:34 -0500

    correction to game

--- src/main/java/io/zipcoder/Game.java
+++ src/main/java/io/zipcoder/Game.java
@@ -24,36 +24,37 @@ public class Game {
         int playerTurn = 0;
         Card temp = null;
         int allAceFound = 0;
-        int round = 0;
         boolean keepGoing = true;
-        while(keepGoing){
+        while(playerTurn < numOfPlayers) {
+            while (!players[playerTurn].getAceStatus()) {
 
-            if(!players[playerTurn].getAceStatus()) {
                 temp = gameDeck.drawCard();
+               // temp.printCard();
                 players[playerTurn].addCard(temp);
+
+                if (temp.getRank().equals("Ace")) {
+                    players[playerTurn].changeAceFound();
+                    allAceFound++;
+
+                }
+                //round++;
             }
-            if(temp.getSuit().equals("Ace")){
-                players[playerTurn].changeAceFound();
-                allAceFound++;
-            }
-            if(gameDeck.getDeck().isEmpty()){//if for whatever reason an ace doesn't appear in the deck
+            if (gameDeck.getDeck().isEmpty()) {//if for whatever reason an ace doesn't appear in the deck
                 break;
             }
             playerTurn++;
-            if(playerTurn>=numOfPlayers){
-                playerTurn = 0;
-            }
-            round++;
-            System.out.println("end of round "+round+ " results: ");//there is something not working correctly
-            for(int i = 0; i < numOfPlayers; i++){
-                players[i].calcScore();
-                System.out.println("player #"+i+" score is: "+players[i].getScore());
-            }
+
             if(allAceFound == numOfPlayers){
                 //keepGoing = false;
                 break;
             }
         }
+            for(int i = 0; i < numOfPlayers; i++){
+                players[i].calcScore();
+                System.out.println("player #"+i+" score is: "+players[i].getScore());
+            }
+
+
         int max = 0;
         Human winner = null;
         for(int i = numOfPlayers-1; i>=0; i--){
